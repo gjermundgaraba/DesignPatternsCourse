2640263db57fa75aecb4b0d53985ad832ecac501
Gjermund Bjaanes <bjaanes@gmail.com>
2016-06-05 15:30:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Design Patterns Course
+
+Code from 3 Java Design Pattern Courses on Pluralsight
+
+* https://www.pluralsight.com/courses/design-patterns-java-creational
+* https://www.pluralsight.com/courses/design-patterns-java-structural
+* https://www.pluralsight.com/courses/design-patterns-java-behavioral
---
+++ src/com/gjermundbjaanes/singleton/DbSingleton.java
@@ -0,0 +1,17 @@
+package com.gjermundbjaanes.singleton;
+
+public class DbSingleton {
+
+    private static DbSingleton instance = null;
+
+    private DbSingleton() {}
+
+    public static DbSingleton getInstance() {
+        if (instance == null) {
+            instance = new DbSingleton();
+        }
+
+        return instance;
+    }
+
+}
