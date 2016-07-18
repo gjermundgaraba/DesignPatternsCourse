35511cd377d91f6b006eb4483240dfaa1a91bf9d
Gjermund Bjaanes <bjaanes@gmail.com>
2016-07-18 08:58:46 +0200

    Builder examples

---
+++ src/com/gjermundbjaanes/builder/LunchOrderBeanDemo.java
@@ -0,0 +1,15 @@
+package com.gjermundbjaanes.builder;
+
+public class LunchOrderBeanDemo {
+
+    public static void main(String[] args) {
+        LunchOrderBean lunchOrderBean = new LunchOrderBean();
+
+        lunchOrderBean.setBread("Wheat");
+        lunchOrderBean.setCondiments("Lettuce");
+        lunchOrderBean.setDressing("Mustard");
+        lunchOrderBean.setMeat("Ham");
+
+    }
+
+}
