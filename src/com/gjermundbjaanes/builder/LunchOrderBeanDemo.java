b85b8a1acd3acaa2f38d1aa4524e7bbe5ca2f05e
Gjermund Bjaanes <bjaanes@gmail.com>
2016-07-20 17:14:25 +0200

    Prototype pattern

--- src/com/gjermundbjaanes/builder/LunchOrderBeanDemo.java
+++ src/com/gjermundbjaanes/builder/LunchOrderBeanDemo.java
@@ -10,6 +10,11 @@ public class LunchOrderBeanDemo {
         lunchOrderBean.setDressing("Mustard");
         lunchOrderBean.setMeat("Ham");
 
+        System.out.println(lunchOrderBean.getBread());
+        System.out.println(lunchOrderBean.getCondiments());
+        System.out.println(lunchOrderBean.getDressing());
+        System.out.println(lunchOrderBean.getMeat());
+
     }
 
 }
---
+++ src/com/gjermundbjaanes/prototype/Registry.java
@@ -0,0 +1,39 @@
+package com.gjermundbjaanes.prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registry {
+
+    private Map<String, Item> items = new HashMap<>();
+
+    public Registry() {
+        loadPrototypes();
+    }
+
+    public Item createItem (String type) {
+        Item item = null;
+
+        try {
+            item = (Item) items.get(type).clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return item;
+    }
+
+    private void loadPrototypes() {
+        Movie movie = new Movie();
+        movie.setTitle("Basic movie");
+        movie.setPrice(24.99);
+        movie.setRuntime("2 hours");
+        items.put("Movie", movie);
+
+        Book book = new Book();
+        book.setNumberOfPages(335);
+        book.setPrice(19.99);
+        book.setTitle("Basic book");
+        items.put("Book", book);
+    }
+}
