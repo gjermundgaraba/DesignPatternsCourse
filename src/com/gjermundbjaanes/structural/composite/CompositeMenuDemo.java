970da1435ecb5dfeef2940af73a81574d13c9a64
Gjermund Bjaanes <bjaanes@gmail.com>
2016-12-08 07:57:43 +0100

    Added composite pattern

---
+++ src/com/gjermundbjaanes/structural/composite/CompositeMenuDemo.java
@@ -0,0 +1,19 @@
+package com.gjermundbjaanes.structural.composite;
+
+public class CompositeMenuDemo {
+
+    public static void main(String[] args) {
+        Menu mainMenu = new Menu("Main", "/main");
+
+        MenuItem safetyMenuItem = new MenuItem("Safety", "/safety");
+        mainMenu.add(safetyMenuItem);
+
+        Menu claimsSubMenu = new Menu("Claims", "/claims");
+        mainMenu.add(claimsSubMenu);
+
+        MenuItem personalClaimsMenu = new MenuItem("Personal Claim", "/personalClaim");
+        claimsSubMenu.add(personalClaimsMenu);
+
+        System.out.println(mainMenu.toString());
+    }
+}
